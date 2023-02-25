d7117b292857ca5b584e141c2f1bc44557830ebe
dimsgusticode <dimsgusticode@gmail.com>
2023-02-26 02:06:44 +0800

    read local database

--- infoinkelas/src/components/table.tsx
+++ infoinkelas/src/components/table.tsx
@@ -1,5 +1,7 @@
 import { BiEdit, BiTrashAlt } from 'react-icons/bi';
 
+import data from '../../database/data.json';
+
 export default function Table(){
     return(
         <table className="min-w-full table-auto">
@@ -23,25 +25,33 @@ export default function Table(){
                 </tr>
             </thead>
             <tbody className="bg-gray-200">
-                <tr className="bg-gray-50 text-center">
+                {
+                    data.map((obj, i) => <Tr {...obj}key={i}/>)
+                }
+            </tbody>
+        </table>
+    )
+}
+
+function Tr({ id, title, description, duedate, status }){
+    return(
+        <tr className="bg-gray-50 text-center">
                     <td className="px-16 py-2 flex-row items-center">
-                        <span className="text-center ml-2 font-semibold">Berita harian</span>
+                        <span className="text-center ml-2 font-semibold">{title || "Unknown"}</span>
                     </td>
                     <td className="px-16 py-2">
-                        <span>Catatan kecil</span>
+                        <span>{description || "Unknown"}</span>
                     </td>
                     <td className="px-16 py-2">
-                        <span>12-12-2012</span>
+                        <span>{duedate || "Unknown"}</span>
                     </td>
                     <td className="px-16 py-2">
-                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-md">Selesai</span></button>
+                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-md">{status || "Unknown"}</span></button>
                     </td>
                     <td className='px-16 py-2 flex justify-around gap-5'>
                         <button className='cursor'><BiEdit size={25} color={"rgb(34,197,94"}></BiEdit></button>
                         <button className='cursor'><BiTrashAlt size={25} color={"rgb(244,63,94"}></BiTrashAlt></button>
                     </td>
                 </tr>
-            </tbody>
-        </table>
     )
 }
