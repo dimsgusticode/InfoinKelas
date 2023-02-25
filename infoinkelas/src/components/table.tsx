86cab4448b5449b90e083818a3c128311e0db08d
dimsgusticode <dimsgusticode@gmail.com>
2023-02-26 01:37:32 +0800

    error and success form

--- infoinkelas/src/components/table.tsx
+++ infoinkelas/src/components/table.tsx
@@ -6,35 +6,35 @@ export default function Table(){
             <thead>
                 <tr className="bg-gray-800">
                     <th className="px-16 py-2">
-                        <span className="text-gray-200">Title</span>
+                        <span className="text-gray-200">Judul</span>
                     </th>
                     <th className="px-16 py-2">
-                        <span className="text-gray-200">Description</span>
+                        <span className="text-gray-200">Deskripsi</span>
                     </th>
                     <th className="px-16 py-2">
-                        <span className="text-gray-200">Due-date</span>
+                        <span className="text-gray-200">Batas waktu</span>
                     </th>
                     <th className="px-16 py-2">
                         <span className="text-gray-200">Status</span>
                     </th>
                     <th className="px-16 py-2">
-                        <span className="text-gray-200">Action</span>
+                        <span className="text-gray-200">Aksi</span>
                     </th>
                 </tr>
             </thead>
             <tbody className="bg-gray-200">
                 <tr className="bg-gray-50 text-center">
                     <td className="px-16 py-2 flex-row items-center">
-                        <span className="text-center ml-2 font-semibold">Daily News</span>
+                        <span className="text-center ml-2 font-semibold">Berita harian</span>
                     </td>
                     <td className="px-16 py-2">
-                        <span>Quicknote</span>
+                        <span>Catatan kecil</span>
                     </td>
                     <td className="px-16 py-2">
                         <span>12-12-2012</span>
                     </td>
                     <td className="px-16 py-2">
-                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-md">Completed</span></button>
+                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-md">Selesai</span></button>
                     </td>
                     <td className='px-16 py-2 flex justify-around gap-5'>
                         <button className='cursor'><BiEdit size={25} color={"rgb(34,197,94"}></BiEdit></button>
